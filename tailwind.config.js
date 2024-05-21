ddedd29db00b3a4f4548c3728b08c4cb0547cab9
MOOD MNKY <simeon.bowman@moodmnky.com>
2024-05-21 11:27:18 -0400

    self-host oauth test

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,26 @@ module.exports = {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
+        custom: {
+          'bg-color': "var(--custom-bg-color)",
+          'panel-color': "var(--custom-panel-color)",
+          'box-shadow': "var(--custom-box-shadow)",
+          'color': "var(--custom-color)",
+          'color-brand': "var(--custom-color-brand)",
+          'color-secondary': "var(--custom-color-secondary)",
+          'border': "var(--custom-border)",
+          'border-radius': "var(--custom-border-radius)",
+          'spacing': "var(--custom-spacing)",
+        },
+      },
+      fontFamily: {
+        custom: "var(--custom-font-family)",
+      },
+      boxShadow: {
+        custom: "var(--custom-box-shadow)",
+      },
+      borderRadius: {
+        custom: "var(--custom-border-radius)",
       },
     },
   },
